5c5316ba86bc15186e7d7adb61cefe38c23427a1
Femi Olah <olahfemi@gmail.com>
2022-09-29 01:04:31 +0100

    First commit

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity =0.7.6;
+pragma solidity ^0.8.0;
 pragma abicoder v2;
 
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
 contract SimpleSwap {
-   constructor() {
+   ISwapRouter public immutable swapRouter;
+   address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+   address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+   uint24 public constant feeTier = 3000;
+
+   constructor(ISwapRouter _swapRouter) {
+      swapRouter = _swapRouter;
+   }
+
+   function swapETHForDAI(uint amountIn) external returns (uint256 amountOut) {
+      // Transfer the specified amount of WETH9 to this contract.
+      TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountIn);
+
+      // Approve the router to spend WETH9.
+      TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
    }
 }
